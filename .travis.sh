bdc34aec7091f8f28d9635fafd01016bdb633084
Foster McLane <fkmclane@gmail.com>
2017-10-08 11:01:05 -0400

    [travis] add testing travis script

---
+++ .travis.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+mirror="https://mirrors.kernel.org/gentoo"
+overlay="$(pwd)"
+root="/gentoo"
+repoman=""
+
+# prepare
+apt-get update
+apt-get install -y curl
+
+# get root image
+mkdir "$root"
+curl -s "$mirror"/releases/amd64/autobuilds/latest-stage4-amd64-minimal.txt | sed -e '1 d' -e '2 d' -e '3 s/\(.*\) .*/\1/' | xargs -I '{}' curl -s "$mirror"/releases/amd64/autobuilds/'{}' | tar -C "$root" -xj
+
+# move repository to image
+cd "$root"/root
+mv "$overlay" "$root"/root/overlay
+
+# mount everything
+sudo mount --bind "$root" "$root"
+sudo mount -t proc none "$root"/proc
+sudo mount --bind /sys "$root"/sys
+sudo mount --make-rslave "$root"/sys
+sudo mount --bind /dev "$root"/dev
+sudo mount --make-rslave "$root"/dev
+
+# merge repoman
+chroot /root/gentoo/root/overlay /bin/bash -c "emerge repoman"
+
+# run repoman on codebase
+exec chroot /root/gentoo/root/overlay /bin/bash -c "repoman scan $overlay"
