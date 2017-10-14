f3b0b25c8c9868b654b1b3482c3d13d96ab5ae5e
Foster McLane <fkmclane@gmail.com>
2017-10-14 19:18:04 -0400

    [ci] source profile before doing anything in chroot

--- .travis.sh
+++ .travis.sh
@@ -2,34 +2,40 @@
 mirror="https://mirrors.kernel.org/gentoo"
 overlay="$(pwd)"
 root="/gentoo"
-repoman=""
 
-# prepare
-apt-get update
-apt-get install -y curl
+strap() {
+	mkdir -p "$1"
+	curl -s "$2"/releases/amd64/autobuilds/latest-stage4-amd64-minimal.txt | sed -e '1 d' -e '2 d' -e '3 s/\(.*\) .*/\1/' | xargs -I '{}' curl -s "$2"/releases/amd64/autobuilds/'{}' | tar -C "$1" -xj
+}
+
+run() {
+	chroot "$1" /bin/bash -c "source /etc/profile && ${@:2}"
+}
+
+prep() {
+	mount --bind "$1" "$1"
+	mount -t proc none "$1"/proc
+	mount --bind /sys "$1"/sys
+	mount --make-rslave "$1"/sys
+	mount --bind /dev "$1"/dev
+	mount --make-rslave "$1"/dev
+
+	run "$1" emerge-webrsync
+	run "$1" eselect profile set default/linux/amd64/13.0
+}
 
 # get root image
-mkdir "$root"
-curl -s "$mirror"/releases/amd64/autobuilds/latest-stage4-amd64-minimal.txt | sed -e '1 d' -e '2 d' -e '3 s/\(.*\) .*/\1/' | xargs -I '{}' curl -s "$mirror"/releases/amd64/autobuilds/'{}' | tar -C "$root" -xj
+strap "$root" "$mirror"
+
+# prepare
+prep "$root"
 
 # move repository to image
 cd "$root"/root
 mv "$overlay" "$root"/root/overlay
 
-# mount everything
-sudo mount --bind "$root" "$root"
-sudo mount -t proc none "$root"/proc
-sudo mount --bind /sys "$root"/sys
-sudo mount --make-rslave "$root"/sys
-sudo mount --bind /dev "$root"/dev
-sudo mount --make-rslave "$root"/dev
-
-# setup system
-chroot "$root" /bin/bash -c "emerge-webrsync"
-chroot "$root" /bin/bash -c "eselect profile set default/linux/amd64/13.0"
-
 # merge repoman
-chroot "$root" /bin/bash -c "emerge repoman"
+run "$root" emerge repoman
 
 # run repoman on codebase
-exec chroot "$root" /bin/bash -c "repoman scan /root/overlay"
+exec run "$root" repoman scan /root/overlay
