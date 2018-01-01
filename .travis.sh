361bb14286d6ab5ec9586080d75aa66c8144371c
Foster McLane <fkmclane@gmail.com>
2018-01-01 04:30:36 -0500

    [ci] mount /dev/shm and split up packages more

--- .travis.sh
+++ .travis.sh
@@ -27,6 +27,8 @@ prep() {
 	mount --make-rslave "$1"/sys
 	mount --bind /dev "$1"/dev
 	mount --make-rslave "$1"/dev
+	mount --types tmpfs --options nosuid,nodev,noexec shm /dev/shm
+	chmod 1777 /dev/shm
 	{ set +x; } 2>/dev/null
 
 	run "$1" emerge-webrsync
