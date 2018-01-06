39de3e56ae4df148acdcd0a72c7b36dd8916c42a
Foster McLane <fkmclane@gmail.com>
2018-01-06 06:39:25 -0500

    [ci] add travis folds to sync

--- .ci/sync.sh
+++ .ci/sync.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 source "$(readlink -f "$(dirname $0)")"/common.sh
+
+echo "travis_fold:start:system.sync"
 set -x
-
 [ -e "$root"/dev ] || prep
-
 pip install awscli
 aws s3 sync "$sync" "$root" --exclude /dev --exclude /sys --exclude /proc --exclude /run/shm
+{ set +x; } 2>/dev/null
+echo "travis_fold:end:system.sync"
