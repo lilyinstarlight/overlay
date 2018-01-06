e087a00b096a9246d40014162c29218bf7d9c4db
Foster McLane <fkmclane@gmail.com>
2018-01-06 09:13:36 -0500

    [ci] split into sync and upload

--- .ci/sync.sh
+++ .ci/sync.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 source "$(readlink -f "$(dirname $0)")"/common.sh
 
+# install AWS CLI and sync with remote
 echo "travis_fold:start:system.sync"
 set -x
-[ -e "$root"/dev ] || prep
+prep
+
 pip install awscli
 aws s3 sync "$sync" "$root" --exclude /dev --exclude /sys --exclude /proc --exclude /run/shm
 { set +x; } 2>/dev/null
---
+++ .ci/upload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+source "$(readlink -f "$(dirname $0)")"/common.sh
+
+# install AWS CLI and upload to remote
+echo "travis_fold:start:system.upload"
+set -x
+pip install awscli
+aws s3 sync "$root" "$sync" --exclude /dev --exclude /sys --exclude /proc --exclude /run/shm
+{ set +x; } 2>/dev/null
+echo "travis_fold:end:system.upload"
