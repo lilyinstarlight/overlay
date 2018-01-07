e831dddfd136a693e871aef64013c0997e4ed9cb
Foster McLane <fkmclane@gmail.com>
2018-01-07 18:56:46 -0500

    [ci] fix issues with excluding directories with awscli

--- .ci/sync.sh
+++ .ci/sync.sh
@@ -7,6 +7,6 @@ set -x
 prep
 
 pip install awscli
-aws s3 sync "$sync" "$root" --exclude "$root"/dev --exclude "$root"/sys --exclude "$root"/proc --exclude "$root"/run/shm
+aws s3 sync "$sync" "$root" --exclude 'dev/*' --exclude 'sys/*' --exclude 'proc/*' --exclude 'run/shm/*'
 { set +x; } 2>/dev/null
 echo "travis_fold:end:system.sync"
--- .ci/upload.sh
+++ .ci/upload.sh
@@ -5,6 +5,6 @@ source "$(readlink -f "$(dirname $0)")"/common.sh
 echo "travis_fold:start:system.upload"
 set -x
 pip install awscli
-aws s3 sync "$root" "$sync" --exclude "$root"/dev --exclude "$root"/sys --exclude "$root"/proc --exclude "$root"/run/shm
+aws s3 sync "$root" "$sync" --exclude 'dev/*' --exclude 'sys/*' --exclude 'proc/*' --exclude 'run/shm/*'
 { set +x; } 2>/dev/null
 echo "travis_fold:end:system.upload"
