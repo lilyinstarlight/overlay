372560cf079efd4d211614a6543ea336aff4bad8
Foster McLane <fkmclane@gmail.com>
2018-01-08 07:12:35 -0500

    [ci] fix creating upload tar to AWS

--- .ci/upload.sh
+++ .ci/upload.sh
@@ -5,7 +5,7 @@ source "$(readlink -f "$(dirname $0)")"/common.sh
 echo "travis_fold:start:system.upload"
 set -x
 pip install awscli
-tar cjpf "$root"/../prefix-"$id".tar.bz2 --xattrs-include='*.*' --numeric-owner --one-file-system "$root/*"
+tar cjpf "$root"/../prefix-"$id".tar.bz2 --xattrs-include='*.*' --numeric-owner --one-file-system -C "$root" "$(ls -A "$root")"
 aws s3 cp "$root"/../prefix-"$id".tar.bz2 "$sync"/prefix-"$id".tar.bz2
 { set +x; } 2>/dev/null
 echo "travis_fold:end:system.upload"
