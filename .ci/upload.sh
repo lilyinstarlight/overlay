f7b8bd29e5c333a57174106446484f5cfbbdc846
Foster McLane <fkmclane@gmail.com>
2018-02-25 23:17:28 -0500

    [ci] fix upload tar

--- .ci/upload.sh
+++ .ci/upload.sh
@@ -5,7 +5,7 @@ source "$(readlink -f "$(dirname $0)")"/common.sh
 echo "travis_fold:start:system.upload"
 set -x
 pip install awscli
-tar cjpf "$root"/../prefix-"$id".tar.bz2 --xattrs-include='*.*' --numeric-owner --one-file-system -C "$root" "$(ls -A "$root")"
+tar cjpf "$root"/../prefix-"$id".tar.bz2 --xattrs-include='*.*' --numeric-owner --one-file-system -C "$root" $(find "$root" -maxdepth 1 -type d -not -name dev -not -name sys -not -name proc -not -name tmp -not -name run -not -name boot | tr '\n' ' ')
 aws s3 cp "$root"/../prefix-"$id".tar.bz2 "$sync"/prefix-"$id".tar.bz2
 { set +x; } 2>/dev/null
 echo "travis_fold:end:system.upload"
