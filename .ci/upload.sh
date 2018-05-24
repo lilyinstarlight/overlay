4d8595f4c537ee4ad0485f3e0afe96e31c82f078
Foster McLane <fkmclane@gmail.com>
2018-05-24 17:11:43 -0400

    [ci] upload all files fixing missing lib link

--- .ci/upload.sh
+++ .ci/upload.sh
@@ -5,7 +5,7 @@ source "$(readlink -f "$(dirname $0)")"/common.sh
 echo "travis_fold:start:system.upload"
 set -x
 pip install awscli
-tar cjpf "$root"/../prefix-"$id".tar.bz2 --xattrs-include='*.*' --numeric-owner --one-file-system -C "$root" $(find "$root" -maxdepth 1 -type d -not -wholename "$root" -not -name dev -not -name sys -not -name proc -printf '%f ')
+tar cjpf "$root"/../prefix-"$id".tar.bz2 --xattrs-include='*.*' --numeric-owner --one-file-system -C "$root" $(find "$root" -maxdepth 1 -not -wholename "$root" -not -name dev -not -name sys -not -name proc -printf '%f ')
 aws s3 cp "$root"/../prefix-"$id".tar.bz2 "$sync"/prefix-"$id".tar.bz2
 { set +x; } 2>/dev/null
 echo "travis_fold:end:system.upload"
